3340aee4e1256a8f6d80599d09d37dacf53f3867
Li Hsin Chen <lihsin0510@gwu.edu>
2019-10-27 15:57:50 -0400

    Add files via upload

---
+++ AmazonProductPage.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 25 22:59:30 2018
+
+@author: Li Hsin
+"""
+#%%
+from lxml import html
+import requests
+#%%
+class AmazonProductPage:
+    def __init__(self, asin, country, user_agent):
+        self.amazon_url  = self.get_url(country) + '/dp/' + asin
+        self.headers = {'User-Agent': user_agent}
+        self.parser = self.get_RankAndRating_page()
+        
+# =============================================================================
+    def get_url(self,country):
+        if country == "us":
+            return 'http://www.amazon.com'
+        elif country == "uk" or country == "jp":
+            return 'http://www.amazon.co.' + country
+        else:
+            return 'http://www.amazon.' + country
+        
+    def robot_check(self):
+        is_robot = self.parser.xpath('//head//title//text()')
+        return is_robot
+
+    def get_RankAndRating_page(self):
+        page = requests.get(self.amazon_url, headers = self.headers, verify=False)
+        page_response = page.text
+        return html.fromstring(page_response)
+    
+    def ExtractNumber(self, string):
+            decode = list(map(lambda x: ord(x), string))
+            num_code = [chr(num) for num in decode if num>47 and num<58]
+            return int("".join(num_code)) 
+
+# =============================================================================
+    def get_product_info(self):
+        ret = {"Item Name": self.get_product_name(), 
+                "Total Reviews": self.get_total_number_reviews(), 
+                "Average Rating": self.get_average_rating(), 
+                "Ranking": self.get_ranking()[0], 
+                "Ranking Category": self.get_ranking()[1],
+                "Fit Recommendation (%)": self.get_fitting_recommendation()}
+        ret.update(self.get_product_stars_dist())
+        return ret
+    
+# =============================================================================
+    def get_product_name(self):
+        XPATH_PRODUCT_NAME = '//h1//span[@id="productTitle"]//text()'
+        raw_product_name = self.parser.xpath(XPATH_PRODUCT_NAME)
+        product_name = ''.join(raw_product_name).strip()
+        return product_name
+    
+    def get_total_number_reviews(self):
+        XPATH_AGGREGATE = '//span[@id="acrCustomerReviewText"]//text()'
+        raw_aggregate = self.parser.xpath(XPATH_AGGREGATE)
+        if raw_aggregate:
+            aggregate_review = self.ExtractNumber(raw_aggregate[0])
+        else:
+            aggregate_review  = 0
+        return aggregate_review
+    
+    def get_product_stars_dist(self):
+        XPATH_AGGREGATE_RATING = '//table[@id="histogramTable"]//tr'
+        total_ratings  = self.parser.xpath(XPATH_AGGREGATE_RATING)
+        ratings_dict = {'5 Star' : 0, '4 Star' : 0, '3 Star' : 0, '2 Star' : 0, '1 Star' : 0}
+        star = 5
+        for ratings in total_ratings:
+            extracted_rating = ratings.xpath('.//text()')
+            if extracted_rating:
+                rating_key = str(star) + " Star"
+                raw_raing_value = [r for r in extracted_rating if "%" in r][-1]
+                rating_value = float(raw_raing_value.strip(" \n").strip("%"))/100
+                if rating_key:
+                    ratings_dict.update({rating_key:rating_value})
+            star -= 1
+        return ratings_dict
+
+
+    def get_average_rating(self):
+        XPATH_AVE_RATING = '//span[@id="acrPopover"]//span//a//i//span//text()'
+        raw_ave_rating = self.parser.xpath(XPATH_AVE_RATING)
+        if raw_ave_rating:
+            if country == 'jp':
+                integer = raw_ave_rating[0].split('.')[0][-1]
+                decimal = raw_ave_rating[0].split('.')[1][-1]
+                ave_rating = float(".".join([integer, decimal]))
+            else:
+                try:
+                    ave_rating = float(raw_ave_rating[0][0:3])
+                except:
+                    ave_rating = float(".".join(raw_ave_rating[0][0:3].split(",")))
+        else:
+            ave_rating = None
+        return ave_rating
+
+    def get_ranking(self):
+        try:
+            XPATH_RANKING = '//li[@id="SalesRank"]//text()'
+            raw_ranking = self.parser.xpath(XPATH_RANKING)
+            ranking = self.ExtractNumber(raw_ranking[2])
+            ranking_category = raw_ranking[2]
+        except IndexError:
+            try:
+                XPATH_RANKING_table = '//table[@id="productDetails_detailBullets_sections1"]//tr'
+                raw_ranking_cell = self.parser.xpath(XPATH_RANKING_table)[-1]
+                raw_ranking = raw_ranking_cell.xpath(".//span//span//text()")[0]         
+                ranking = self.ExtractNumber(raw_ranking)
+                ranking_category = raw_ranking
+            except:
+                ranking = None
+                ranking_category = None
+
+        return [ranking, ranking_category]
+
+
+    def get_fitting_recommendation(self):
+        XPATH_fitRecommendation = '//span[@id="fitRecommendationsLinkRatingText"]//text()'
+        raw_fitRecommendation = self.parser.xpath(XPATH_fitRecommendation)
+        
+        if raw_fitRecommendation:
+            fitRecommendation = float(raw_fitRecommendation[0].split('(')[1].split("%")[0])
+        else:
+            fitRecommendation = None
+        return fitRecommendation
+
+
+# =============================================================================
+    def get_bulletpoint(self):
+        XPATH_BULLETPOINT = '//div[@id="feature-bullets"]//text()'
+        raw_bulletpoint = self.parser.xpath(XPATH_BULLETPOINT)
+        
+        step1 = [string.replace("\n","").replace("\t","").lstrip() for string in raw_bulletpoint]
+        step2 = [s for s in step1 if s is not ""]
+        
+        return step2
+
+
+#%%
+asin = "B07KYSCDWV"
+country = "us"
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134"
+
+
+product = AmazonProductPage(asin,country,user_agent)
+product.robot_check()
+product.get_product_info()
+
+
+
+
+
+
+
+
+
+
+
---
+++ AmazonSearchPage.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 11 11:09:01 2019
+
+@author: Li Hsin
+"""
+
+import requests
+from datetime import datetime as dt
+from lxml import html
+
+
+class AmazonSearchPage():
+    def __init__(self,keyword,user_agent):
+        self.headers = {'User-Agent': user_agent}
+        self.keyword = keyword
+        self.parser = self.get_search_page()
+
+    def robot_check(self):
+        is_robot = self.parser.xpath('//head//title//text()')
+        return is_robot
+    
+# =============================================================================
+    def get_url(self):
+        url = 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords='
+        nkws = self.keyword.replace(" ","+")
+        return url + nkws
+        
+    def get_search_page(self):
+        amazon_url = self.get_url()
+        page = requests.get(amazon_url, headers = self.headers, verify=False)
+        page_response = page.text
+        return html.fromstring(page_response)
+
+    
+# =============================================================================
+    def get_product_list(self):
+        XPATH_PRODUCT = '//div[@data-index]'
+        raw_product_list = self.parser.xpath(XPATH_PRODUCT)
+        return raw_product_list
+
+    
+# =============================================================================
+    def get_product(self,item):
+        ad_type = item.xpath('.//div[@class="sg-col-inner"]/div/div/span[@class="a-size-base a-color-secondary"]/text()')
+        ad_type1 = ".".join(ad_type)
+        
+        # =============================================================================
+        # Ads Type                
+        # =============================================================================
+        if not ad_type1:
+            ad_type1 = 'Organic'
+        
+        # =============================================================================
+        # Brand Name
+        # =============================================================================
+        try:
+            raw_brand_name = item.xpath('.//div/div/div/div[@class="a-row a-size-base a-color-secondary"]/h5/span//text()')
+            brand_name = raw_brand_name [0]
+        except:
+            brand_name = ""
+        
+        # =============================================================================
+        # Product Name
+        # =============================================================================
+        raw_product_name = item.xpath('.//div/div/div/h2/a/span/text()')
+        product_name = ''.join(raw_product_name).strip()
+        product_name = brand_name + " " + product_name
+
+        # =============================================================================
+        # Price
+        # =============================================================================
+        raw_price = item.xpath('.//div/a/span[@class="a-price"]/span/text()')
+        product_price = ''.join(raw_price).strip()
+        
+
+
+        # =============================================================================
+        # Product Rating
+        # =============================================================================
+        try:            
+            raw_rating = item.xpath('.//a/i/span[@class="a-icon-alt"]/text()')
+            product_rating = float(''.join(raw_rating).strip().split(" ")[0])
+        except:
+            product_rating = None
+            
+        # =============================================================================
+        # Products Total Review            
+        # =============================================================================
+        try:
+            raw_total_review = item.xpath('.//span[@class="a-size-base"]/text()')
+            total_review = int(raw_total_review[0])
+        except:
+            total_review = None
+        time = str(dt.today())
+  
+        return {"Keyword": self.keyword,
+                "Brand Name": brand_name,
+                "Product Name": product_name,
+                "ASIN": item.values()[0],
+                "position": int(item.values()[1]),
+                "Price": product_price,
+                "Rating": product_rating,
+                "Total Reviews": total_review,
+                "Time": time,
+                "Type":ad_type1}
+    
+ 
+# =============================================================================
+    def get_product_info_list(self):
+        raw_product_list = self.get_product_list()
+        result = []
+        for item in raw_product_list:
+            newData = self.get_product(item)
+            result.append(newData)
+        return result
+
+# =============================================================================
+    def get_headline_info(self):
+        XPATH_BRAND_NAME = '//div/span[@id="hsaSponsoredByBrandName"]/text()'
+        raw_brand_name = self.parser.xpath(XPATH_BRAND_NAME)
+        brand_name = ''.join(raw_brand_name).strip()
+        time = str(dt.today())
+        
+        return {"Keyword": self.keyword,
+                "Sponsored Brand": brand_name,
+                "Time": time,
+                "Period": self.time_period(time)}
+#%%
+keyword = "halloween customs for women"
+user_agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+
+search_page = AmazonSearchPage(keyword, user_agent)
+search_page.robot_check()
+product_list = search_page.get_product_info_list()
+product_list[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
